56b7ef52021dc35c2f64f02490f6e83473ad0186
jarekboho <123318901+jarekboho@users.noreply.github.com>
2025-08-16 02:43:49 +0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -110,7 +110,7 @@ class Character extends FlxSprite
 				normalChar = new FlxSprite();
 				normalChar.frames = Paths.getSparrowAtlas('GF_assets', 'shared');
 				normalChar.animation.addByPrefix('cheer', 'GF Cheer', 24, false);
-				animation.addByPrefix('combo50', 'GF Cheer', 24, false);
+				normalChar.animation.addByPrefix('combo50', 'GF Cheer', 24, false);
 				normalChar.animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 				normalChar.animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 				normalChar.animation.addByPrefix('scared', 'GF FEAR', 24);
